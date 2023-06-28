0dd07c76b7351e63d5a8a1c037d98ff6688f2751
Alex Kim <ajk8710@nyu.edu>
2023-06-28 19:07:20 -0400

    Spring Boot Setup but error due to not having entityManagerFactory

---
+++ day12-springboot1/src/main/java/com/synergisticit/Day12Springboot1Application.java
@@ -0,0 +1,13 @@
+package com.synergisticit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12Springboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12Springboot1Application.class, args);
+	}
+
+}
