44d129d0cb3a3821ff8d758ba7ba3f176fd1a34e
Alex Kim <ajk8710@nyu.edu>
2023-07-05 21:36:57 -0400

    Spring Boot: CRUD operation for User with JpaRepository interface

---
+++ day17-assignment3/src/main/java/com/synergisticit/Day17Assignment3Application.java
@@ -0,0 +1,13 @@
+package com.synergisticit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day17Assignment3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day17Assignment3Application.class, args);
+	}
+
+}
